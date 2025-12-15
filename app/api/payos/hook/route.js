a399f4642935a7f38d6f608bcd25ab96a7cae7ef
Nguyễn Quang Quân <haizdwng@gmail.com>
2025-12-15 21:46:16 +0700

    Refactor route.js by removing order handling code
    Removed order processing logic and database connections.

--- app/api/payos/hook/route.js
+++ app/api/payos/hook/route.js
@@ -19,47 +19,7 @@ export async function POST(req) {
       );
     }
 
-    await connectDB();
-
-    const order = await Order.findOne({ code: data.orderCode });
-
-    if (!order) {
-      return NextResponse.json(
-        { error: 'Không tìm thấy đơn hàng' },
-        { status: 404 }
-      );
-    }
-
-    if (data.code !== '00' && data.code !== '02') {
-      return NextResponse.json(
-        { message: 'Không cần cập nhật đơn hàng' },
-        { status: 200 }
-      )
-    } else if (data.code === '02') {
-      order.status = 'canceled';
-      await order.save();
-      return NextResponse.json(
-        { message: 'Đơn hàng đã bị hủy' },
-        { status: 200 }
-      );
-    } else if (data.code === '00') {
-
-      order.status = 'completed';
-      await order.save();
-
-      const book = await Book.findById(order.bookId);
-      if (book) {
-        if (!book.bought.includes(order.userId)) {
-          book.bought.push(order.userId);
-        }
-        book.sold = (book.sold || 0) + 1;
-        await book.save();
-      }
-
-      const user = await User.findById(order.userId);
-
-      await shareFileWithUser(book.id, user.email);
-    };
+    
 
     return NextResponse.json({
       message: 'Cập nhật đơn hàng thành công',
